350e005867feb4a3603b32b3e9d849f4ec1163cb
Sebastian Kerz <52817143+SebKerz@users.noreply.github.com>
2024-08-12 11:46:42 +0200

    Delete data directory

--- data/ros/git_lfs_pull.sh
+++
@@ -1,9 +0,0 @@
-function lfs_pull(){
-    if [ -z ${1} ]; then echo "please provide directory name";return; fi;
-    cur_dir=$(pwd)
-    { cd ${1} 
-    git lfs install
-    git lfs pull
-    } || {  }
-    cd ${cur_dir}
-}
--- data/ros/source_tokens.sh
+++
@@ -1,7 +0,0 @@
-if [ -n "${KB_SCRIPT_PATH}" ]; then
-{
-  source "${KB_SCRIPT_PATH}/access_tokens" > /dev/null
-} || { 
-  echo "could not get access tokens" 
-}
-fi
